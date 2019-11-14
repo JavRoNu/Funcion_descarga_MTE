0bcd1f7a9c2064885e126de5f2ff4ffb8f01ad81
rodríguez núñez javier luis <javierluis.rodriguez@rai.usc.es>
2019-11-14 18:32:15 +0100

    actualización

--- mte_ap.R
+++ mte_ap.R
@@ -3,15 +3,13 @@
 
 library(rvest)
 library(magrittr)
-
+library(stringr)
 
 usuario <- "javierluis.rodriguez"
 contra <- "xa1234bi"
 
-
 url <- "http://eio.usc.es/pub/mte/index.php/es/acceder"
 ses <- html_session(url,encoding = "UTF-8")
-
 
 f.log <- ses %>%
   html_node("form") %>%
@@ -47,11 +45,9 @@ cat(
 )
 
 pick.asig <- readline("Selecciona asignatura/s :") %>%
-str_extract_all(,pattern = "[0-9]{1,2}") %>%
+str_extract_all(pattern = "[0-9]{1,2}") %>%
   unlist() %>%
   as.numeric()
-
-
 
 
 # error en el nº de asignaturas
@@ -63,10 +59,16 @@ str_extract_all(,pattern = "[0-9]{1,2}") %>%
 f.asig <- user.asig %>%
   html_nodes("form") %>%
   html_form() %>%
-  '[['(2) %>%
-  set_values("nom_materia" = f.asig$fields$nom_materia$options[i + 1])
+  '[['(2) 
+
+f.asig <-
+  set_values(f.asig, "nom_materia" = f.asig$fields$nom_materia$options[i + 1])
 
 a <- submit_form(user.asig, f.asig, "submit")
+
+
+# hay que sacar las fechas
+#fechas <- a %>% html_node(xpath = '//*[@id="li_4"]/table[2]')%>% html_attr("td")# %>% html_table(fill = T) 
 
 t.mat <- a %>% read_html(encoding = "UTF-8") %>%
   html_nodes(xpath = '//*[@id="li_4"]/table[2]') %>% html_nodes("a")
@@ -75,6 +77,7 @@ t.mat <- a %>% read_html(encoding = "UTF-8") %>%
 disp <- t.mat %>% html_text() %>% trimws()
 link <- t.mat %>% html_attr("href")
 nms <- sapply(strsplit(link,split = "Mat_[0-9]{1,}_?[0-9]{1,}_"), function(x) x[2])
+
 # falta añadir las fechas de incorporación del arhcivo
 
 
@@ -84,8 +87,10 @@ cat(
   as.character(Sys.Date()),
   " : \n",
   paste0(" ", seq(length(disp)), " --- ", disp, "\n")
-# descarga
+
 )
+
+# descarga
 
 dwn <- jump_to(a,link[2]) 
 download.file(dwn$url,disp[2],mode = "wb")
